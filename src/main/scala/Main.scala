01e8d80c8eedf80438ab172fed78071ebe817724
samyeuh <samybouhamidi2@gmail.com>
2025-01-22 16:35:03 +0100

    console

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,27 @@
 package fr.efrei.fp.project
 
+import scala.io.StdIn.readLine
 import SQLContext._
 
 object Main extends App {
-  execute {
-//    "create table Students (Name, Age, Grade)".exec()
-//    "Students add (Alice, 22, A)".exec()
-//    "Students add (Bob, 18, B)".exec()
-//    "Students select (Name, Grade)".exec()
-      "Students filter (Age <= 20)".exec()
+  println("Bienvenue dans le terminal interactif SQL-like !")
+  println("Tapez une commande ou 'exit' pour quitter.\n")
+
+  var continue = true
+
+  while (continue) {
+    print(">>> ")
+    val command = readLine().trim
+
+    command match {
+      case "exit" =>
+        continue = false
+        println("Fermeture du terminal. À bientôt !")
+      case cmd if cmd.nonEmpty =>
+        val result = CommandProcessor.execute(cmd)
+        println(result)
+      case _ =>
+        println("Commande vide. Veuillez entrer une commande.")
+    }
   }
 }
