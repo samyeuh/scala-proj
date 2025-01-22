55e249a27aab1f5fcbd7f0946d649282c050c0fe
CyrilTakam <127041301+JOJO182003@users.noreply.github.com>
2025-01-22 18:16:28 +0100

    Update Main.scala
    Exeption handling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,25 +2,24 @@ package fr.efrei.fp.project
 
 import scala.io.StdIn.readLine
 
+/**
+ * Entry point for the application.
+ */
 object Main extends App {
-  println("Bienvenue dans le terminal interactif SQL-like !")
-  println("Tapez une commande ou 'exit' pour quitter.\n")
+  println("Bienvenue dans le terminal interactif SQL-like ! Tapez une commande ou 'exit' pour quitter.")
 
   var continue = true
 
   while (continue) {
     print(">>> ")
-    val command = readLine().trim
-
+    val command = scala.io.StdIn.readLine().trim
     command match {
       case "exit" =>
         continue = false
         println("Fermeture du terminal. À bientôt !")
       case cmd if cmd.nonEmpty =>
-        val result = CommandProcessor.execute(cmd)
-        println(result)
-      case _ =>
-        println("Commande vide. Veuillez entrer une commande.")
+        println(CommandProcessor.execute(cmd))
+      case _ => println("Commande vide. Veuillez entrer une commande.")
     }
   }
 }
